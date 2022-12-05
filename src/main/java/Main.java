6159572bda1a054e3e147ef9007bc2b9c1ab401e
Maciej <walder_m@wp.pl>
2022-12-05 20:05:07 +0100

    W tym commit dodałem deposit i dodałem ją do kalsy main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,16 +12,32 @@ public class Main {
         String nazwisko=scanner.nextLine();
         System.out.println("Wprowadź nr PESEL:");
         int pesel=scanner.nextInt();
-        System.out.println("Witaj w naszym MENU:\n1. Informacje o rachunku bankowym.\n2.Wpłata pieniędzy (zasilenie konta).\n3.Wypłata na konto.\n4. Wyjście");
+        System.out.println("Witaj w naszym MENU:\n1. Informacje o rachunku bankowym.\n2. Wpłata pieniędzy (zasilenie konta).\n3. Wypłata na konto.\n4. Wyjście");
         User user1=new User(imie, nazwisko, pesel);
+        System.out.println("Co bys chciał zrobic, wybierz z menu:");
+        int choose =scanner.nextInt();
+        switch (choose){
+            case 1:
+                System.out.println("Informacje o koncie");
+                user1.userDetails();
+                break;
+            case 2:
+                System.out.println("Ile chesz wpłacić pieniędzy");
+                double moneyToDeposit= scanner.nextDouble();
+                user1.deposit(moneyToDeposit);
+                System.out.println("Saldo twojego konta wynosi: "+user1.getBalance());
 
+                break;
 
+            case 3:
+                System.out.println("Ilość wypłaconych pieniedzy");
+                break;
 
+            case 4:
+                System.out.println("Wyjscie z aplikacji");
+                break;
 
-
-
-
-
+        }
 
 
     }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -6,26 +6,38 @@ public class User {
     private String surename;
     private String login;
     private int pesel;
-    private int accaunt;
-    private double balance ;
+    private int account;
+    private double balance;
 
-    public User(String name,String surename, int pesel){
-        this.name=name;
-        this.surename =surename;
-        this.pesel=pesel;
-        this.loginGenerator(name, surename);
-        this.balance=100;
-        this.accaunt =1;
+    public User(String name, String surename, int pesel) {
+        this.name = name;
+        this.surename = surename;
+        this.pesel = pesel;
+        this.login = loginGenerator(name, surename);
+        this.balance = 100;
+        this.account = 1;
 
     }
 
     public String loginGenerator(String name, String surename) {
-        return(name.substring(0,3)+surename.substring(0,3));
+        return (name.substring(0, 3) + surename.substring(0, 3));
 
     }
 
+    public void userDetails() {
+        System.out.println("Nr. konta: " + account + " Imię " + name + " Nazwisko: " + surename + " Nick Name: " + login + " PESEL: " + pesel + " Saldo: " + balance);
+    }
 
+    public double deposit(double money) {
+        return balance=balance+money;
+    }
+    public double getBalance(){
+        return balance;
+    }
 }
+
+
+
 
 
 
