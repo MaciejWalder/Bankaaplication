4682403f355a92ca419ba65c11dafc66d4db773a
Maciej <walder_m@wp.pl>
2022-12-05 20:58:41 +0100

    dodanie do/while do całego projektu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,32 +12,42 @@ public class Main {
         String nazwisko=scanner.nextLine();
         System.out.println("Wprowadź nr PESEL:");
         int pesel=scanner.nextInt();
-        System.out.println("Witaj w naszym MENU:\n1. Informacje o rachunku bankowym.\n2. Wpłata pieniędzy (zasilenie konta).\n3. Wypłata na konto.\n4. Wyjście");
+        int choose;
+        do{
+        System.out.println("\nWitaj w naszym MENU:\n1. Informacje o rachunku bankowym.\n2. Wpłata pieniędzy (zasilenie konta).\n3. Wypłata na konto.\n4. Wyjście");
         User user1=new User(imie, nazwisko, pesel);
-        System.out.println("Co bys chciał zrobic, wybierz z menu:");
-        int choose =scanner.nextInt();
-        switch (choose){
-            case 1:
-                System.out.println("Informacje o koncie");
-                user1.userDetails();
-                break;
-            case 2:
-                System.out.println("Ile chesz wpłacić pieniędzy");
-                double moneyToDeposit= scanner.nextDouble();
-                user1.deposit(moneyToDeposit);
-                System.out.println("Saldo twojego konta wynosi: "+user1.getBalance());
 
-                break;
+           System.out.println("Co bys chciał zrobic, wybierz z menu:");
 
-            case 3:
-                System.out.println("Ilość wypłaconych pieniedzy");
-                break;
+           choose = scanner.nextInt();
+           switch (choose) {
+               case 1:
+                   System.out.println("Informacje o koncie");
+                   user1.userDetails();
+                   break;
+               case 2:
+                   System.out.println("Ile chesz wpłacić pieniędzy");
+                   double moneyToDeposit = scanner.nextDouble();
+                   user1.deposit(moneyToDeposit);
+                   System.out.println("Saldo twojego konta wynosi: " + user1.getBalance());
 
-            case 4:
-                System.out.println("Wyjscie z aplikacji");
-                break;
+                   break;
 
-        }
+               case 3:
+                   System.out.println("Podaj kwotę którą chcesz wypłacić:");
+                   double withdrawMoney = scanner.nextDouble();
+                   user1.withdraw(withdrawMoney);
+                   System.out.println("Kwota wypłacona: " + withdrawMoney + "\nKwota, która pozostała Ci na koncie " + user1.getBalance());
+
+
+                   break;
+
+               case 4:
+                   System.out.println("Do zobaczenia");
+                   break;
+
+           }
+       }while(choose!=4);
 
 
     }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -18,23 +18,37 @@ public class User {
         this.account = 1;
 
     }
-
+    //realizacja loginu do logowania
     public String loginGenerator(String name, String surename) {
         return (name.substring(0, 3) + surename.substring(0, 3));
-
     }
-
     public void userDetails() {
         System.out.println("Nr. konta: " + account + " Imię " + name + " Nazwisko: " + surename + " Nick Name: " + login + " PESEL: " + pesel + " Saldo: " + balance);
     }
-
+    //tworzenie wpłaty
     public double deposit(double money) {
-        return balance=balance+money;
+        return balance = balance + money;
     }
-    public double getBalance(){
+    public double getBalance() {
         return balance;
     }
+    //tworzenie wypłaty
+
+    public void withdraw(double money1) {
+
+        if (money1 > balance) {
+            System.out.println("Kwota którą wypłacasz jest większa niż dostępne środki na konie.");
+        } else {
+            balance = balance - money1;
+
+
+        }
+    }
 }
+
+
+
+
 
 
 
